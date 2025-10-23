525b69acbed15c97db6ef7b2477cb962840cd477
EarlyGreyman <147036156+EarlyGreyman@users.noreply.github.com>
2025-10-23 21:22:04 +0200

    generalised app for CLI input

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import word_count, char_count, sort_dict
+import sys
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -7,18 +8,24 @@ def get_book_text(filepath):
     return file_content
 
 def main():
+    # Checking if filepath to book is provided
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        filepath = sys.argv[1]
+
     # Declaring and importing book for analysis
-    filepath = "books/frankenstein.txt"
     book = get_book_text(filepath)
 
     # Get character list in form {"char": "b", "num": 4868}
     char_list = sort_dict(char_count(book))
 
     # Print nice report
-    print("============ BOOKBOT ============"
-          f"Analyzing book found at {filepath}"
-          "----------- Word Count ----------"
-          f"Found {word_count(book)} total words"
+    print("============ BOOKBOT ============\n"
+          f"Analyzing book found at {filepath}\n"
+          "----------- Word Count ----------\n"
+          f"Found {word_count(book)} total words\n"
           f"--------- Character Count -------")
 
     for dict in char_list:
